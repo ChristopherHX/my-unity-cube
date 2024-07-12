8e64164112beb59c46da1be3ffec66ac919ffb2f
Christopher Homberger <christopher.homberger@web.de>
2024-07-12 16:08:03 +0200

    ...

--- Assets/SmartphoneRemote.cs
+++ Assets/SmartphoneRemote.cs
@@ -20,6 +20,7 @@ public class SmartphoneRemote : ScriptableObject
     private TcpListener listener = null;
     public Vector3 accel = new Vector3();
     public Vector3 gyro = new Vector3();
+    public Vector3 position = new Vector3();
     public int Port = 0;
 
     private CancellationTokenSource cancellationToken = null;
@@ -53,18 +54,19 @@ public class SmartphoneRemote : ScriptableObject
 
     private async void ProcessInputs(Socket s) {
         while(!cancellationToken.IsCancellationRequested) {
-            byte[] bytes = new byte[4 * 6];
+            byte[] bytes = new byte[4 * 9];
             int received = s.Receive(bytes);
             if(received != bytes.Length) {
                 break;
             }
-            float[] xyz = new float[6];
-            for(int i = 0; i < 6; i++) {
+            float[] xyz = new float[9];
+            for(int i = 0; i < 9; i++) {
                 Array.Reverse(bytes, i * 4, 4);
                 xyz[i] = BitConverter.ToSingle(bytes, i * 4);
             }
             accel.Set(xyz[0], xyz[1], xyz[1]);
             gyro.Set(xyz[3], xyz[4], xyz[5]);
+            position.Set(xyz[6], xyz[7], xyz[8]);
             await Task.Delay(10);
         }
     }
